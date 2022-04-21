898a591418f29e73b60e453318a6eb3ae98dd385
Dzmitry <gawrfan@gmail.com>
2022-04-22 02:55:30 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', ''),
+	personalMovieDB = {
+		count: numberOfFilms,
+		movies: {},
+		actors: {},
+		genres: [],
+		privat: false,
+	},
+	q1 = prompt('Один из последних просмотренных фильмов?', ''),
+	q2 = prompt('На сколько оцените его?', ''),
+	q3 = prompt('Один из последних просмотренных фильмов?', ''),
+	q4 = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies = { [q1]: q2, [q3]: q4 };
+
+console.log(personalMovieDB);
